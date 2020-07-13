c9f7784b8c1d9e00180647b169d6538200bc4a54
pratik432 <pratiksingh432@gmail.com>
2020-07-14 03:06:57 +0530

    Goal 4 completed

--- RDIP/Codes/Forming_sent.js
+++ RDIP/Codes/Forming_sent.js
@@ -1,25 +1,140 @@
-var json='{"English":['+'{"1":["John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple"]},'+
-   '{"2":["some students like to study in the night","at night some students like to study"]},'+
-   '{"3":["John and Mary went to church","Mary and John went to church"]},'+'{"4":["John went to church after eating","after eating John went to church","John after eating went to church"]},'+'{"5":["did he go to market","he did go to market"]},'+'{"6":["the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics"]},'+
-   '{"7":["John goes to the library and studies","John studies and goes to the library"]},'+
-   '{"8":["John ate an apple so did she","she ate an apple so did John"]},'+'{"9":["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book"]},'+
-   '{"10":["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her"}]}'; '"Hindi":['+
-    '{"1":["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम"]},'+
-    '{"2":["राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी"]},'+
-    '{"3":["मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम"]},'+
-    '{"4":["राम खाकर सोया","खाकर राम सोया","राम सोया खाकर","खाकर सोया राम","सोया राम खाकर","सोया खाकर राम"]},'+
-    '{"5":["बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"]},'+
-    '{"6":["एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब"]},'+
-    '{"7":["एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है"," बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब"," है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"}]}';
 
 function selectlang(){
+var English_Sentence = 
+
+[["John ate an apple before afternoon ",
+"before afternoon John ate an apple ", 
+"John before afternoon ate an apple "], 
+["some students like to study in the night ",
+"at night some students like to study "], 
+["John and Mary went to church ", 
+"Mary and John went to church "], 
+["John went to church after eating ", 
+"after eating John went to church ", 
+"John after eating went to church "], 
+["did he go to market ", 
+"he did go to market "], 
+["the woman who called my sister sells cosmetics ",
+"the woman who sells cosmetics called my sister ",
+"my sister who sells cosmetics called the woman ",
+"my sister who called the woman sells cosmetics "],
+["John goes to the library and studies ",
+"John studies and goes to the library "],
+["John ate an apple so did she ",
+"she ate an apple so did John "],
+["the teacher returned the book after she noticed the error ", 
+"the teacher noticed the error after she returned the book ",
+"after the teacher returned the book she noticed the error ",
+"after the teacher noticed the error she returned the book ", 
+"she returned the book after the teacher noticed the error ", 
+"she noticed the error after the teacher returned the book ",
+"after she returned the book the teacher noticed the error ",
+"after she noticed the error the teacher returned the book "],
+["I told her that I bought a book yesterday ",
+"I told her yesterday that I bought a book ", 
+"yesterday I told her that I bought a book ", 
+"I bought a book that I told her yesterday ", 
+"I bought a book yesterday that I told her ", 
+"yesterday I bought a book that I told her "]];
+
+ var  Hindi_Sentence = 
+[["राम और श्याम बाजार गयें ",
+"राम और श्याम गयें बाजार ", 
+"बाजार गयें राम और श्याम ", 
+"गयें बाजार राम और श्याम "],
+["राम सोया और श्याम भी ", 
+"श्याम सोया और राम भी ", 
+"सोया श्याम और राम भी ", 
+"सोया राम और श्याम भी "],
+["मैंने उसे बताया कि राम सो रहा है ",
+"मैंने उसे बताया कि सो रहा है राम ",
+"उसे मैंने बताया कि राम सो रहा है ",
+"उसे मैंने बताया कि सो रहा है राम ",
+"मैंने बताया उसे कि राम सो रहा है ",
+"मैंने बताया उसे कि सो रहा है राम ",
+"उसे बताया मैंने कि राम सो रहा है ", 
+"उसे बताया मैंने कि सो रहा है राम ", 
+"बताया मैंने उसे कि राम सो रहा है ",
+"बताया मैंने उसे कि सो रहा है राम ", 
+"बताया उसे मैंने कि राम सो रहा है ",
+"बताया उसे मैंने कि सो रहा है राम "],
+["राम खाकर सोया ",
+"खाकर राम सोया ",
+"राम सोया खाकर ",
+"खाकर सोया राम ",
+"सोया राम खाकर ",
+"सोया खाकर राम "],
+["बिल्लियों को मारकर कुत्ता सो गया ",
+"मारकर बिल्लियों को कुत्ता सो गया ",
+"बिल्लियों को मारकर सो गया कुत्ता ", 
+"मारकर बिल्लियों को सो गया कुत्ता ", 
+"कुत्ता सो गया बिल्लियों को मारकर ", 
+"कुत्ता सो गया मारकर बिल्लियों को ", 
+"सो गया कुत्ता बिल्लियों को मारकर ", 
+"सो गया कुत्ता मारकर बिल्लियों को "],
+["एक लाल किताब वहाँ है ", 
+"एक लाल किताब है वहाँ ", 
+"वहाँ है एक लाल किताब ", 
+"है वहाँ एक लाल किताब "],
+["एक बड़ी सी किताब वहाँ है ", 
+"एक बड़ी सी किताब है वहाँ ", 
+"बड़ी सी एक किताब वहाँ है ", 
+"बड़ी सी एक किताब है वहाँ ", 
+"वहाँ है एक बड़ी सी किताब ", 
+"वहाँ है बड़ी सी एक किताब ", 
+"है वहाँ एक बड़ी सी किताब ", 
+"है वहाँ बड़ी सी एक किताब "]];
     var x = document.getElementById("langselect").value;
     document.getElementById('form_1').innerHTML="Form a sentence (Declarative or Interrogative or any other type) from the given words."
     document.getElementById('form_2').innerHTML="(select the buttons in proper order)"
-    console.log(x);
-    if(x == "null")
+    document.getElementById('form_3').innerHTML=""
+    if(x == "english")
+    {
+      var grpsent = English_Sentence[Math.floor(Math.random()*English_Sentence.length)] ;
+      var sent = grpsent[Math.floor(Math.random()*grpsent.length)];
+      var word = sent.split(" ");
+        var len = word.length;
+          word.pop();
+      for(initial=0;initial<len;initial++)
+      {
+
+        var position = Math.floor(Math.random()*word.length);
+        var temp = word[position];
+        word[position] = word[word.length-1];
+        word[word.length-1] = temp;
+         var words = document.createElement("BUTTON"); 
+         words.style.marginLeft = "30px"
+         words.innerHTML = word[word.length-1];
+      document.getElementById("form_3").appendChild(words);
+}
+     
+    }
+    else if (x == "hindi")
+    {
+      var grpsenth = Hindi_Sentence[Math.floor(Math.random()*Hindi_Sentence.length)] ;
+      var senth = grpsenth[Math.floor(Math.random()*grpsenth.length)];
+      var wordh = senth.split(" ");
+     var len = wordh.length;
+      wordh.pop();
+      for(initial=0;initial<len;initial++)
+      {
+
+        var position = Math.floor(Math.random()*wordh.length);
+        var temp = wordh[position];
+        wordh[position] = wordh[wordh.length-1];
+        wordh[wordh.length-1] = temp;
+         var words = document.createElement("BUTTON"); 
+         words.style.marginLeft = "30px"
+         words.style.margin = "center"
+         words.innerHTML = wordh[wordh.length-1];
+      document.getElementById("form_3").appendChild(words);
+}
+
+    }
+    else
     {
         alert("Select Language");
         return false;
     }
 }
+
